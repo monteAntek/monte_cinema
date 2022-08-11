a3dab4fe178289d873b9c3b12d56e3ec49f563f5
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-11 14:22:41 +0200

    add vite-svg-loader, add global mixins file import

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import svgLoader from 'vite-svg-loader'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
-      sass: {
-        additionalData: `@import "@/assets/styles/_variables"`
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/_variables";
+          @import "@/assets/styles/_mixins";
+        `
       }
     }
   },
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
