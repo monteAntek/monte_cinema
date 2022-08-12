c258e69efc67bd5c0ee0c29cfe70a9b669a4910b
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-12 14:39:23 +0200

    create HomePage component file + import & render Navbar component

---
+++ src/pages/home-page.vue
@@ -0,0 +1,26 @@
+<script>
+    import { defineComponent } from 'vue';
+    import NavBar from '../components/global/nav-bar/nav-bar.vue';
+    export default defineComponent({
+        components: {
+            NavBar,
+        },
+        name: 'HomePage',
+        props: {},
+        data() {
+            return {}
+        },
+    });
+</script>
+
+<template>
+    <div class="homepage">
+        <NavBar />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .homepage {
+        margin: 0 auto;
+    }
+</style>
