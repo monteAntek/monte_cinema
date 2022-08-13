abf7f47cf27d3e88a388f1b33452a19a3f080839
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-13 11:09:00 +0200

    update import path with @ alias + updated BaseButton warning generating :is attribute

--- src/components/global/nav-bar/nav-button-container.vue
+++ src/components/global/nav-bar/nav-button-container.vue
@@ -1,7 +1,7 @@
 <script>
     import { defineComponent } from 'vue';
 
-    import BaseButton from '../base-button.vue';
+    import BaseButton from '@/components/global/base-button.vue';
 
     export default defineComponent({
         name: 'NavButtonContainer',
@@ -13,8 +13,8 @@
 
 <template>
     <div class="nav-button-container">
-        <BaseButton class="nav-register-button" :is="a" label="Register" colorScheme="redTextOnly" size="40" />
-        <BaseButton class="nav-register-link" :is="a" label="Login" colorScheme="redFill" size="40" />
+        <BaseButton class="nav-register-button" :is="'a'" label="Register" colorScheme="redTextOnly" size="40" />
+        <BaseButton class="nav-register-link" :is="'a'" label="Login" colorScheme="redFill" size="40" />
     </div>
 </template>
 
