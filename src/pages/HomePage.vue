cc6a75f9dd58ffe439c1bdc1fd96dd2d36920663
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-14 18:35:25 +0200

    rename file + update import paths + remove indentations

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from 'vue';
+
+import HeroSection from '@/components/HomePage/HeroSection/HeroSection.vue';
+
+export default defineComponent({
+    name: 'HomePage',
+    components: {
+        HeroSection,
+    },
+    props: {},
+    data() {
+        return {}
+    },
+});
+</script>
+
+<template>
+    <div class="homepage">
+        <HeroSection />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.homepage {
+    margin: 0 48px;
+    @include screen-small {
+        margin: 0 48px;
+    @include screen-small {
+        margin: 0 auto;
+    }
+    }
+}
+</style>
