e99c6e54cc9d52897e29a722d9c1d1eb9445ec95
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-14 18:37:49 +0200

    rename file + update import paths + remove indentations

---
+++ src/components/global/NavBar/NavButtonContainer.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from 'vue';
+
+import BaseButton from '@/components/global/BaseButton.vue';
+
+export default defineComponent({
+    name: 'NavButtonContainer',
+    components: {
+        BaseButton,
+    },
+});
+</script>
+
+<template>
+    <div class="nav-button-container">
+        <BaseButton class="nav-register-button" is="a" label="Register" colorScheme="redTextOnly" size="40" />
+        <BaseButton class="nav-register-link" is="a" label="Login" colorScheme="redFill" size="40" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-button-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 16px;
+    background-color: $color-snow-white;
+    @include screen-small {
+        flex-direction: column;
+        width: 100%;
+    }
+}
+</style>
