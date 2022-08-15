959d922634574e158fc6391e2945ab88c8641d1c
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-15 14:53:28 +0200

    import & render MoviesSection in HomePage template

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,11 +2,13 @@
 import { defineComponent } from 'vue';
 
 import HeroSection from '@/components/HomePage/HeroSection/HeroSection.vue';
+import MoviesSection from '@/components/HomePage/MoviesSection/MoviesSection.vue';
 
 export default defineComponent({
     name: 'HomePage',
     components: {
         HeroSection,
+        MoviesSection,
     },
     props: {},
     data() {
@@ -18,6 +20,7 @@ export default defineComponent({
 <template>
     <div class="homepage">
         <HeroSection />
+        <MoviesSection />
     </div>
 </template>
 
