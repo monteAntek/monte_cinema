b3f827f0b77fb8a915a275b7c03713373c21d93c
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-17 21:35:24 +0200

    move HomePage component to views folder

---
+++ src/views/HomePage.vue
@@ -0,0 +1,37 @@
+<script>
+import { defineComponent } from 'vue';
+
+import HeroSection from '@/components/HomePage/HeroSection/HeroSection.vue';
+import MoviesSection from '@/components/HomePage/MoviesSection/MoviesSection.vue';
+
+export default defineComponent({
+    name: 'HomePage',
+    components: {
+        HeroSection,
+        MoviesSection,
+    },
+    props: {},
+    data() {
+        return {}
+    },
+});
+</script>
+
+<template>
+    <div class="homepage">
+        <HeroSection />
+        <MoviesSection />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.homepage {
+    margin: 0 48px;
+    @include screen-small {
+        margin: 0 48px;
+    @include screen-small {
+        margin: 0 auto;
+    }
+    }
+}
+</style>
