1144d0a19c18dfa8c4275c0e60bc50a3df604a5c
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-17 21:02:44 +0200

    add link wrapper & fix styling to ensure correct component display

--- src/components/global/NavBar/NavLInks.vue
+++ src/components/global/NavBar/NavLInks.vue
@@ -3,47 +3,64 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
    name: 'NavLinks',
-   data() {
-    return {
-        urls: [
-            {id: 1, displayName: 'Movies', url: '#'},
-            {id: 2, displayName: 'Screenings', url: '#'},
-            {id: 3, displayName: 'Contact us', url: '#'},
-        ],
-    };
-   },
+    data() {
+        return {
+            urls: [
+                {id: 1, displayName: 'Movies', url: '#'},
+                {id: 2, displayName: 'Screenings', url: '#'},
+                {id: 3, displayName: 'Contact us', url: '#'},
+            ],
+        }
+    },
 });
 </script>
+
 <template>
     <div class="navbar__links">
-        <a class="navbar__link" v-for="url in urls" :key="url.id" :href="url.url">{{url.displayName}}</a>
+        <div class="navbar__link-wrapper" v-for="url in urls" :key="url.id">
+            <a class="navbar__link" :href="url.url">{{url.displayName}}</a>
+        </div>
     </div>
 </template>
+
 <style lang="scss" scoped>
 .navbar__links {
+    align-self: stretch;
     display: flex;
+    justify-content: center;
+    align-items: center;
     gap: 10px;
     background-color: $color-snow-white;
+    
     @include screen-small {
         flex-direction: column;
         width: 100%;
     }
 
-    .navbar__link {
-        text-decoration: none;
-        font-family: $font-roboto;
-        font-size: $fs-18;
-        color: $color-jumbo-gray;
-        padding: 24px;
+    .navbar__link-wrapper {
+        align-self: stretch;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: stretch;
         border-bottom: 5px solid transparent;
         &:hover {
             cursor: pointer;
             border-bottom: 5px solid $color-cherry-red;
         }
-        @include screen-small {
-            width: 100%;
+        .navbar__link {
+            display: block;
+            text-decoration: none;
+            font-family: $font-roboto;
+            font-size: $fs-18;
+            color: $color-jumbo-gray;
+            padding: 24px;
+            
+           
+            @include screen-small {
+                width: 100%;
+            }
         }
     }
-}
-    
+} 
 </style>
