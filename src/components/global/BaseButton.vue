087053f768afdd8666179569c5b3afad45cc9fbe
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:04:12 +0200

    update props + add computed classes for size & colorScheme + update button / router-link render logic + style refactor for BEM compliance

--- src/components/global/BaseButton.vue
+++ src/components/global/BaseButton.vue
@@ -2,98 +2,132 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'BaseButton',
-    props: {
-        tag: {
-            type: String,
-            default: 'a',
-        },
-        label: {
-            type: String,
-            required: true,
-        },
-        url: {
-            type: String,
-            default: '#'
-        },
-        colorScheme: {
-            type: String,
-            default: 'redFill',
-        },
-        size: {
-            type: String,
-            required: true,
-        },
+  name: 'BaseButton',
+  props: {
+    type: {
+      type: String,
+      default: 'button'
     },
-    emits: ['click'],
+    to: {
+      type: Object,
+      default: { name: 'Home' }
+    },
+    colorScheme: {
+      type: String,
+      validator(value) {
+        return [
+          'red-fill',
+          'red-border',
+          'pink-border',
+          'gray-fill',
+          'gray-border',
+          'red-text',
+          'gray-text'
+        ].includes(value);
+      },
+      default: 'red-fill'
+    },
+    size: {
+      type: String,
+      validator(value) {
+        return ['small', 'medium', 'large', 'x-large'].includes(value);
+      },
+      default: 'x-large'
+    }
+  },
+  computed: {
+    buttonClasses() {
+      return [
+        'button',
+        { 'button--red-fill': this.colorScheme === 'red-fill' },
+        { 'button--red-border': this.colorScheme === 'red-border' },
+        { 'button--pink-border': this.colorScheme === 'pink-border' },
+        { 'button--gray-fill': this.colorScheme === 'gray-fill' },
+        { 'button--gray-border': this.colorScheme === 'gray-border' },
+        { 'button--red-text': this.colorScheme === 'red-text' },
+        { 'button--gray-text': this.colorScheme === 'gray-text' },
+        { 'button--small': this.size === 'small' },
+        { 'button--medium': this.size === 'medium' },
+        { 'button--large': this.size === 'large' },
+        { 'button--x-large': this.size === 'x-large' }
+      ];
+    }
+  },
+  emits: ['click']
 });
 </script>
 
 <template>
-    <component 
-        class="base-button"
-        :is="tag"
-        :url="url"
-        :colorScheme="colorScheme" 
-        :size="size"
-        @click="$emit('click')"
-        >
-        {{label}}
-    </component>
+  <template v-if="to">
+    <router-link :class="buttonClasses" :colorScheme="colorScheme" :to="to">
+      <slot />
+    </router-link>
+  </template>
+  <template v-else>
+    <button
+      :type="type"
+      :class="buttonClasses"
+      :colorScheme="colorScheme"
+      @click="$emit('click', $event)"
+    >
+      <slot />
+    </button>
+  </template>
 </template>
 
 <style lang="scss" scoped>
-.base-button {
-    border: 2px solid transparent;
-    border-radius: $br-64;
-    text-align: center;
-    font-weight: $fw-500;
-    line-height: 100%;
-    letter-spacing: 0.015em;
-    font-family: $font-roboto-mono;
-    &:hover {
-        cursor: pointer;
-    }
-    @include screen-small {
-        width: 100%;
-    }
-}
+.button {
+  border: 2px solid transparent;
+  border-radius: $br-64;
+  text-align: center;
+  font-weight: $fw-500;
+  line-height: 1;
+  letter-spacing: 0.015em;
+  font-family: $font-roboto-mono;
+  text-decoration: none;
+  display: inline-block;
+  &:hover {
+    cursor: pointer;
+  }
+  @include screen-small {
+    width: 100%;
+  }
 
-.base-button[colorScheme="redFill"] {
+  &--red-fill {
     background-color: $color-cherry-red;
     border-color: $color-cherry-red;
     color: $color-snow-white;
-    &:hover {
-        background-color: $color-totem-pole-red;
-        border-color: $color-bittersweet-red;
-        color: $color-snow-white;
+    &:hover,
+    &:active,
+    &:focus,
+    &:focus-visible {
+      background-color: $color-totem-pole-red;
+      border-color: $color-bittersweet-red;
+      color: $color-snow-white;
     }
-}
+  }
 
-.base-button[colorScheme="redTextOnly"] {
+  &--red-text {
     background-color: transparent;
     border-color: transparent;
     color: $color-cherry-red;
-    &:hover {
-        background-color: $color-wisp-pink;
-        border-color: $color-cherry-red;
+    &:hover,
+    &:active,
+    &:focus,
+    &:focus-visible {
+      background-color: $color-wisp-pink;
+      border-color: $color-cherry-red;
     }
-}
+  }
 
-.base-button[size="40"] {
-    padding: 12px 32px;
-    font-size: $fs-16;
-    
-}
-
-.base-button[size="56"] {
+  &--x-large {
     padding: 19px 40px;
     font-size: $fs-18;
-    
-}
+  }
 
-.base-button[is="a"] {
-    display: inline-block;
+  &--large {
+    padding: 12px 32px;
+    font-size: $fs-16;
+  }
 }
-
 </style>
