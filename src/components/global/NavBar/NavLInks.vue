b81f35cd3ee33b5e763e1c1a710ad3a8157293a3
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:18:23 +0200

    update urls ref for router functionality + refactor class naming & styling for BEM compliance + add router-link-active styling

--- src/components/global/NavBar/NavLInks.vue
+++ src/components/global/NavBar/NavLInks.vue
@@ -2,65 +2,66 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-   name: 'NavLinks',
-    data() {
-        return {
-            urls: [
-                {id: 1, displayName: 'Movies', url: '#'},
-                {id: 2, displayName: 'Screenings', url: '#'},
-                {id: 3, displayName: 'Contact us', url: '#'},
-            ],
-        }
-    },
+  name: 'NavLinks',
+  data() {
+    return {
+      urls: [
+        { id: 1, displayName: 'Movies', routeName: 'Movies' },
+        { id: 2, displayName: 'Screenings', routeName: 'Screenings' },
+        { id: 3, displayName: 'Contact us', routeName: 'Contact' }
+      ]
+    };
+  }
 });
 </script>
 
 <template>
-    <div class="navbar__links">
-        <div class="navbar__link-wrapper" v-for="url in urls" :key="url.id">
-            <a class="navbar__link" :href="url.url">{{url.displayName}}</a>
-        </div>
-    </div>
+  <div class="container">
+    <router-link
+      class="container__link"
+      v-for="url in urls"
+      :key="url.id"
+      :to="{ name: url.routeName }"
+    >
+      {{ url.displayName }}
+    </router-link>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.navbar__links {
-    align-self: stretch;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    background-color: $color-snow-white;
-    
-    @include screen-small {
-        flex-direction: column;
-        width: 100%;
-    }
+.container {
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  background-color: $color-snow-white;
 
-    .navbar__link-wrapper {
-        align-self: stretch;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: stretch;
-        border-bottom: 5px solid transparent;
-        &:hover {
-            cursor: pointer;
-            border-bottom: 5px solid $color-cherry-red;
-        }
-        .navbar__link {
-            display: block;
-            text-decoration: none;
-            font-family: $font-roboto;
-            font-size: $fs-18;
-            color: $color-jumbo-gray;
-            padding: 24px;
-            
-           
-            @include screen-small {
-                width: 100%;
-            }
-        }
+  &__link {
+    align-self: stretch;
+    display: block;
+    text-decoration: none;
+    font-family: $font-roboto;
+    font-size: $fs-18;
+    color: $color-jumbo-gray;
+    padding: 48px 24px 24px 24px;
+    border-bottom: 5px solid transparent;
+    &:hover {
+      cursor: pointer;
+      border-bottom: 5px solid $color-cherry-red;
+
+      @include screen-small {
+        width: 100%;
+      }
     }
-} 
+  }
+  .router-link-active {
+    border-bottom: 5px solid $color-cherry-red;
+  }
+  @include screen-small {
+    flex-direction: column;
+    width: 100%;
+  }
+}
 </style>
