23820bee58ee81a4c29c0231b1e6f0418ffe4d5f
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:12:03 +0200

    refactor class naming & styling for BEM compliance

--- src/components/global/BaseTag.vue
+++ src/components/global/BaseTag.vue
@@ -4,73 +4,83 @@ import { defineComponent } from 'vue';
 import TagEllipse from '@/assets/icons/tag-ellipse.svg';
 
 export default defineComponent({
-    name: 'BaseTag',
-    components: {
-        TagEllipse,
+  name: 'BaseTag',
+  components: {
+    TagEllipse
+  },
+  props: {
+    labels: {
+      type: Array,
+      default: () => ['label']
     },
-    props: {
-        labels: {
-            type: Array,
-            default: () => ['label'],
-        },
-        colorScheme: {
-            validator(value) {
-                return ['red', 'gray', 'transparent'].includes(value);
-            },
-            default: 'red',
-        },
-    },
+    colorScheme: {
+      validator(value) {
+        return ['red', 'gray', 'transparent'].includes(value);
+      },
+      default: 'red'
+    }
+  },
+  computed: {
+    tagLabels() {
+      return [
+        'tag',
+        { 'tag--red': this.colorScheme === 'red' },
+        { 'tag--gray': this.colorScheme === 'gray' },
+        { 'tag--transparent': this.colorScheme === 'transparent' }
+      ];
+    }
+  }
 });
 </script>
 
 <template>
-    <div 
-        class="base-tag"
-        :colorScheme="colorScheme"
+  <div :class="tagLabels" :colorScheme="colorScheme">
+    <div
+      class="tag__label-container"
+      v-for="(label, index) in labels"
+      :key="index"
     >
-        <div class="base-tag-label-container" v-for="(label, index) in labels" :key="index">
-            <span class="base-tag-label">{{label}}</span>
-            <TagEllipse class="base-tag-label-ellipse" v-if="index < labels.length - 1" />
-        </div>
+      <span class="tag__label">{{ label }}</span>
+      <TagEllipse class="tag__label-ellipse" v-if="index < labels.length - 1" />
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.base-tag {
-    flex-shrink: 2;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 10px;
-    padding: 8px 16px;
-    border-radius: $br-64;
-    .base-tag-label-container {
+.tag {
+  flex-shrink: 2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  padding: 8px 16px;
+  border-radius: $br-64;
+  &__label-container {
     display: flex;
     justify-content: space-between;
     align-items: center;
     gap: 8px;
 
-        .base-tag-label {
-            font-family: $font-roboto;
-            font-weight: $fw-700;
-            font-size: $fs-14;
-            line-height: 114%;
-        }
+    .tag__label {
+      font-family: $font-roboto;
+      font-weight: $fw-700;
+      font-size: $fs-14;
+      line-height: 1.14;
     }
-}
-
-.base-tag[colorScheme='red'] { 
+  }
+  &--red {
     background-color: $color-wisp-pink;
     color: $color-bittersweet-red;
-}
+  }
 
-.base-tag[colorScheme='gray'] { 
+  &--gray {
     background-color: $color-athens-gray;
     color: $color-jumbo-gray;
-}
+  }
 
-.base-tag[colorScheme='transparent'] { 
+  &--transparent {
     background-color: transparent;
     color: $color-athens-gray;
+  }
 }
 </style>
