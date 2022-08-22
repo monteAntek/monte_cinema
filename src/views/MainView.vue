5f06ac4389deab24ce6a69916fdf498e7e0c4f71
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 13:46:59 +0200

    create MainView component for router-view rendering

---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainView'
+});
+</script>
+
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
