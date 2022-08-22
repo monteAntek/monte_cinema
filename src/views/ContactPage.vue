58e9c103a5d15ec5dc9a196f84ac9b8f077ac7de
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:22:56 +0200

    create mock components for router testing

---
+++ src/views/ContactPage.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ContactPage'
+});
+</script>
+
+<template>
+  <h1>ContactPage</h1>
+</template>
+
+<style lang="scss" scoped></style>
