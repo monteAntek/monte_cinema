e4c01205ea0a6998f0d8a1587269965f5ff7be88
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:33:27 +0200

    create draft MoviesContainer component for MoviesPage view

---
+++ src/components/MoviesPage/MoviesContainer.vue
@@ -0,0 +1,39 @@
+<script>
+import { defineComponent } from 'vue';
+
+import MovieCard from '@/components/MoviesPage/MovieCard.vue';
+
+export default defineComponent({
+  name: 'MoviesContainer',
+  components: {
+    MovieCard
+  },
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  }
+});
+</script>
+
+<template>
+  <section class="container">
+    <MovieCard
+      v-for="movie in movies"
+      :key="movie.id"
+      :movieId="movie.id"
+      :movieTitle="movie.title"
+      :movieImgUrl="movie.poster_url"
+      :movieLength="movie.length"
+      :movieGenre="movie.genre.name"
+    />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  @include moviesGrid;
+  margin-bottom: 64px;
+}
+</style>
