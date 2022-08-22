0c66c6da09949848f777e8ae34b83e24f4119e2e
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 13:50:38 +0200

    import & initialize moviesStore + add MainView component + remove HomePage view + update styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script>
 import { defineComponent } from 'vue';
 
-import HomePage from '@/views/HomePage.vue';
-import NavBar from '@/components/global/NavBar/NavBar.vue'
+import NavBar from '@/components/global/NavBar/NavBar.vue';
+import MainView from '@/views/MainView.vue';
+
+import { useMoviesStore } from '@/store/movies.js';
 
 export default defineComponent({
   components: {
-    HomePage,
     NavBar,
+    MainView
+  },
+  data() {
+    return {
+      allMovies: useMoviesStore().fetchAllMovies()
+    };
+  },
+  mounted() {
+    this.allMovies;
   }
 });
 </script>
@@ -17,7 +27,7 @@ export default defineComponent({
     <NavBar />
   </header>
   <main>
-    <HomePage />
+    <MainView class="main-view" />
   </main>
 </template>
 
@@ -25,5 +35,12 @@ export default defineComponent({
 main {
   max-width: 1440px;
   margin: 0 auto;
+  .main-view {
+    margin: 0 48px;
+
+    @include screen-small {
+      margin: 0 auto;
+    }
+  }
 }
 </style>
