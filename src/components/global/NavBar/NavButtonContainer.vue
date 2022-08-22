5275af217bca1a26c3f2c5e25630ce6a07ec4be3
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:16:42 +0200

    refactor class naming & styling for BEM compliance

--- src/components/global/NavBar/NavButtonContainer.vue
+++ src/components/global/NavBar/NavButtonContainer.vue
@@ -4,31 +4,48 @@ import { defineComponent } from 'vue';
 import BaseButton from '@/components/global/BaseButton.vue';
 
 export default defineComponent({
-    name: 'NavButtonContainer',
-    components: {
-        BaseButton,
-    },
+  name: 'NavButtonContainer',
+  components: {
+    BaseButton
+  }
 });
 </script>
 
 <template>
-    <div class="nav-button-container">
-        <BaseButton class="nav-register-button" is="a" label="Register" colorScheme="redTextOnly" size="40" />
-        <BaseButton class="nav-register-link" is="a" label="Login" colorScheme="redFill" size="40" />
-    </div>
+  <div class="container">
+    <BaseButton
+      class="container__button"
+      :to="{ name: 'Register' }"
+      colorScheme="red-text"
+      size="large"
+    >
+      Register</BaseButton
+    >
+    <BaseButton
+      class="container__button"
+      :to="{ name: 'Login' }"
+      colorScheme="red-fill"
+      size="large"
+    >
+      Login
+    </BaseButton>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.nav-button-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    gap: 16px;
-    background-color: $color-snow-white;
-    @include screen-small {
-        flex-direction: column;
-        width: 100%;
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+  background-color: $color-snow-white;
+
+  @include screen-small {
+    width: 100%;
+    &__button {
+      margin-bottom: 24px;
     }
+  }
 }
 </style>
