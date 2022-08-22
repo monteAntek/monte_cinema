8bc33b2b11d65b72ee7436e629b896e70545fe27
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:16:42 +0200

    refactor class naming & styling for BEM compliance

--- src/components/global/NavBar/NavBar.vue
+++ src/components/global/NavBar/NavBar.vue
@@ -8,96 +8,98 @@ import NavLinks from '@/components/global/NavBar/NavLinks.vue';
 import NavButtonContainer from '@/components/global/NavBar/NavButtonContainer.vue';
 
 export default defineComponent({
-    name: 'NavBar',
-    components: {
-        MonteCinemaIcon,
-        ToggleNavOpenIcon,
-        ToggleNavClosedIcon,
-        NavLinks,
-        NavButtonContainer,
-    },
-    props: {
-        userType: {
-            validator(value) {
-                return ['public', 'client', 'employee'].includes(value);
-            },
-        },
-    },
-    data() {
-        return {
-            isNavOpen: false,
-        }
-    },
-    methods: {
-        toggleNavOpen() {
-            this.isNavOpen = !this.isNavOpen;
-        },
-    },
+  name: 'NavBar',
+  components: {
+    MonteCinemaIcon,
+    ToggleNavOpenIcon,
+    ToggleNavClosedIcon,
+    NavLinks,
+    NavButtonContainer
+  },
+  props: {
+    userType: {
+      validator(value) {
+        return ['public', 'client', 'employee'].includes(value);
+      }
+    }
+  },
+  data() {
+    return {
+      isNavOpen: false
+    };
+  },
+  methods: {
+    toggleNavOpen() {
+      this.isNavOpen = !this.isNavOpen;
+    }
+  }
 });
 </script>
 
 <template>
-    <nav 
-        class="navbar"
-        :userType="userType" 
-    >
-        <div class="nav-header-small">
-            <div class="nav-monte-logo">
-                <MonteCinemaIcon />
-            </div>
-            <div 
-                class="nav-toggle-icon" 
-                @click="toggleNavOpen"
-            >
-                <ToggleNavOpenIcon v-show="!isNavOpen" />
-                <ToggleNavClosedIcon v-show="isNavOpen" />
-            </div>
-        </div>
-        <NavLinks class="nav-links" :class="{'is-visible' : isNavOpen}" />
-        <NavButtonContainer class="nav-button-container" :class="{'is-visible' : isNavOpen}" />
-    </nav>
+  <nav class="nav" :userType="userType">
+    <div class="nav__header--small">
+      <router-link :to="{ name: 'Home' }" class="nav__logo">
+        <MonteCinemaIcon />
+      </router-link>
+      <div class="nav__toggle-icon" @click="toggleNavOpen">
+        <ToggleNavOpenIcon v-show="!isNavOpen" />
+        <ToggleNavClosedIcon v-show="isNavOpen" />
+      </div>
+    </div>
+    <NavLinks
+      class="nav__links"
+      :class="{ 'nav__links--is-visible': isNavOpen }"
+    />
+    <NavButtonContainer
+      class="nav__actions"
+      :class="{ 'nav__actions--is-visible': isNavOpen }"
+    />
+  </nav>
 </template>
 
 <style lang="scss" scoped>
-.navbar {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 48px;
-    min-height: 112px;
-    background-color: $color-snow-white;
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 48px;
+  min-height: 112px;
+  background-color: $color-snow-white;
 
-    .nav-toggle-icon {
-        display: none;
+  &__toggle-icon {
+    display: none;
+  }
+
+  @include screen-small {
+    flex-direction: column;
+    padding: 0 24px;
+    min-height: 98px;
+
+    &__header--small {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      padding: 30px 24px;
+
+      .nav__toggle-icon {
+        display: block;
+        &:hover {
+          cursor: pointer;
+        }
+      }
     }
-        
-    @include screen-small {
-        flex-direction: column;
-        padding: 0 24px;
-        min-height: 98px;
 
-        .nav-header-small {
-            width: 100%;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            padding: 30px 24px;
-
-            .nav-toggle-icon {
-                display: block;
-                &:hover {
-                    cursor: pointer;
-                }
-            }
-        }
-
-        .nav-links, .nav-button-container {
-            display: none;
-        }
-        .is-visible {
-            display: flex;
-        }
-    }       
+    &__links,
+    &__actions {
+      display: none;
+      &--is-visible {
+        display: block;
+        margin-bottom: 32px;
+      }
+    }
+  }
 }
 </style>
