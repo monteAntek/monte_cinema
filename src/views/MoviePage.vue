357f7903e91b1e71e4819786a8282d07e7a87143
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-22 14:22:56 +0200

    create mock components for router testing

---
+++ src/views/MoviePage.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MoviePage'
+});
+</script>
+
+<template>
+  <h1>MoviePage</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RegisterPage'
+});
+</script>
+
+<template>
+  <h1>RegisterPage</h1>
+</template>
+
+<style lang="scss" scoped></style>
