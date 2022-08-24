2b2f7535e4dcc4dfd1c40d87c46d6a317ff90346
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-24 11:34:06 +0200

    remove mock data from props defaults

--- src/components/global/BaseSelect.vue
+++ src/components/global/BaseSelect.vue
@@ -11,7 +11,9 @@ export default defineComponent({
   props: {
     selectOptions: {
       type: Array,
-      default: ['Option 1', 'Option 2', 'Option 3', 'Option 4']
+      default() {
+        return [];
+      }
     },
     modelValue: {
       type: String,
