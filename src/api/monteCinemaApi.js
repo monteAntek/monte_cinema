7e4e1a982a0dfac259afe4b1e4a02ab426cec07b
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-26 14:13:48 +0200

    add single movie api call

--- src/api/monteCinemaApi.js
+++ src/api/monteCinemaApi.js
@@ -6,3 +6,8 @@ export const getAllMovies = async () => {
   const response = await axios.get('/movies');
   return response.data;
 };
+
+export const getMovieDetails = async (id) => {
+  const response = await axios.get(`/movies/${id}`);
+  return response.data;
+};
