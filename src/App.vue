4b06ba8a8566cb31be8920608a882812c701f034
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-26 14:12:23 +0200

    add draft global error boundary

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ export default defineComponent({
   components: {
     NavBar
   },
+  data() {
+    return {
+      error: null,
+      errorInstance: null,
+      errorInfo: ''
+    };
+  },
   computed: {
     ...mapState(useMoviesStore, ['allMovies', 'genres'])
   },
@@ -19,6 +26,12 @@ export default defineComponent({
   },
   mounted() {
     this.fetchAllMovies();
+  },
+  errorCaptured(err, instance, info) {
+    this.error = err;
+    this.errorInstance = instance;
+    this.errorInfo = info;
+    return false;
   }
 });
 </script>
@@ -27,21 +40,34 @@ export default defineComponent({
   <header>
     <NavBar />
   </header>
-  <main>
-    <router-view class="main-view"></router-view>
-  </main>
+  <template v-if="error">
+    <div class="error">
+      <p>{{ error }}</p>
+      <br />
+      <p>{{ errorInstance }}</p>
+      <br />
+      <p>{{ errorInfo }}</p>
+    </div>
+  </template>
+  <template v-if="!error">
+    <main>
+      <router-view class="main-view"></router-view>
+    </main>
+  </template>
 </template>
 
 <style lang="scss" scoped>
+.error {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 main {
   max-width: 1440px;
-  margin: 0 auto;
-  .main-view {
-    margin: 0 48px;
-
-    @include screen-small {
-      margin: 0 auto;
-    }
+  margin: 0 48px;
+  @include screen-small {
+    margin: 0 auto;
   }
 }
 </style>
