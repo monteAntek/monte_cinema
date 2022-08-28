d1dfdd7c9fa186c9713bc964c53ccec70de94abd
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-08-28 17:31:37 +0200

    create draft DataLoader component

---
+++ src/components/global/DataLoader.vue
@@ -0,0 +1,40 @@
+<script>
+import { defineComponent } from 'vue';
+
+import MonteCinemaIcon from '@/assets/icons/monte-cinema-logo.svg';
+
+export default defineComponent({
+  name: 'DataLoader',
+  components: {
+    MonteCinemaIcon
+  }
+});
+</script>
+
+<template>
+  <div class="container">
+    <MonteCinemaIcon class="container__loader" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__loader {
+    animation: pulse 3s ease-in-out infinite;
+  }
+}
+</style>
