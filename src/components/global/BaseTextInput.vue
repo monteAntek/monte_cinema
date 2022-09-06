057c2f7c42bb7e472405af84d75c5ac43d7e88bd
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-06 20:36:48 +0200

    import & add ViewOff + ViewOn icons for password input
    remove unnecessary props
    
    add toggle password visible functionality
    
    add slots for label & error messages
    
    update class naming & styling

--- src/components/global/BaseTextInput.vue
+++ src/components/global/BaseTextInput.vue
@@ -2,18 +2,18 @@
 import { defineComponent } from 'vue';
 
 import SearchIcon from '@/assets/icons/search-icon.svg';
+import ViewOffIcon from '@/assets/icons/view-off-icon.svg';
+import ViewOnIcon from '@/assets/icons/view-on-icon.svg';
 
 export default defineComponent({
   name: 'BaseTextInput',
   components: {
-    SearchIcon
+    SearchIcon,
+    ViewOffIcon,
+    ViewOnIcon
   },
   props: {
-    inputName: {
-      type: String,
-      default: 'BaseInput'
-    },
-    inputType: {
+    type: {
       type: String,
       default: 'text'
     },
@@ -21,13 +21,28 @@ export default defineComponent({
       type: String,
       default: ''
     },
-    placeholder: {
-      type: String,
-      default: 'placeholder'
-    },
     isSearch: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      isPasswordVisible: false,
+      inputType: this.type
+    };
+  },
+  methods: {
+    toggleIsPasswordVisible() {
+      this.isPasswordVisible = !this.isPasswordVisible;
+      this.inputType === 'text'
+        ? (this.inputType = 'password')
+        : (this.inputType = 'text');
+    }
+  },
+  computed: {
+    inputClasses() {
+      return ['base-input', { 'base-input--hasError': this.$slots.error }];
     }
   },
   emits: ['input', 'update:modelValue']
@@ -35,23 +50,40 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="text-input">
-    <label :for="inputName" class="text-input__label"><slot /></label>
-    <input
-      class="text-input__input"
-      :id="inputName"
-      :type="inputType"
-      :modelValue="modelValue"
-      :placeholder="placeholder"
-      @input="$emit('update:modelValue', $event.target.value)"
-    />
-    <SearchIcon class="text-input--search" v-if="isSearch" />
+  <div :class="inputClasses">
+    <label v-if="$slots.label" :for="$attrs.id" class="base-input__label">
+      <slot name="label" />
+    </label>
+    <div class="base-input__container">
+      <div class="icon-wrapper">
+        <input
+          class="base-input__container__input"
+          v-bind="$attrs"
+          :type="inputType"
+          :modelValue="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+        />
+        <SearchIcon class="base-input__container__search" v-if="isSearch" />
+        <ViewOffIcon
+          class="base-input__container__show-password"
+          v-if="inputType === 'text' && isPasswordVisible"
+          @click="toggleIsPasswordVisible"
+        />
+        <ViewOnIcon
+          class="base-input__container__show-password"
+          v-if="inputType === 'password' && !isPasswordVisible"
+          @click="toggleIsPasswordVisible"
+        />
+      </div>
+      <div v-if="$slots.error" class="base-input__container__error">
+        <slot name="error" />
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.text-input {
-  position: relative;
+.base-input {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -62,16 +94,62 @@ export default defineComponent({
     @include inputLabel;
   }
 
-  &__input {
-    @include inputInput;
-    color: $color-jumbo-gray;
-  }
+  &__container {
+    width: 100%;
 
-  &--search {
-    font-size: $fs-32;
-    position: absolute;
-    right: 3%;
-    top: 48%;
+    .icon-wrapper {
+      position: relative;
+    }
+
+    &__input {
+      position: relative;
+      @include inputInput;
+      color: $color-jumbo-gray;
+
+      &:hover {
+        background: $color-inputHover-gray;
+      }
+
+      &:active,
+      &:focus-within,
+      &:focus-visible {
+        @include inputActive;
+      }
+    }
+
+    &__search,
+    &__show-password {
+      font-size: $fs-32;
+      position: absolute;
+      right: 3%;
+      top: 25%;
+    }
+
+    &__show-password {
+      &:hover {
+        cursor: pointer;
+      }
+    }
+
+    &__error {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      gap: 8px;
+      margin-top: 8px;
+      font-family: $font-roboto;
+      font-weight: $fw-400;
+      font-size: $fs-14;
+      line-height: 1.7;
+      letter-spacing: 0.04em;
+      color: $color-cherry-red;
+    }
+  }
+  &--hasError {
+    &__input {
+      @include inputError;
+    }
   }
 }
 </style>
