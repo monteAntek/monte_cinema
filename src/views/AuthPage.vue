4e082a247a1920dff600545489837cd7977e4cca
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-06 21:17:32 +0200

    create AuthPage view for rendering register & login forms

---
+++ src/views/AuthPage.vue
@@ -0,0 +1,26 @@
+<script>
+import { defineComponent } from 'vue';
+
+import RegisterForm from '@/components/Auth/RegisterForm.vue';
+import LoginForm from '@/components/Auth/LoginForm.vue';
+
+export default defineComponent({
+  name: 'AuthPage',
+  components: { RegisterForm, LoginForm },
+  computed: {
+    currentRouteName() {
+      return this.$route.name;
+    }
+  }
+});
+</script>
+
+<template>
+  <template v-if="currentRouteName === 'Register'">
+    <RegisterForm />
+  </template>
+
+  <template v-if="currentRouteName === 'Login'">
+    <LoginForm />
+  </template>
+</template>
