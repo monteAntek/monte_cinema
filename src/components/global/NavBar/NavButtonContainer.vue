e183c09fbf8a70f71048c7818ea45f87711e1dc1
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-06 20:44:44 +0200

    import userStore & map logout action
    create onLogout method
    
    display different button variants depending on isLoggedIn prop

--- src/components/global/NavBar/NavButtonContainer.vue
+++ src/components/global/NavBar/NavButtonContainer.vue
@@ -1,34 +1,73 @@
 <script>
 import { defineComponent } from 'vue';
 
+import { mapActions } from 'pinia';
+import { useAuthStore } from '@/store/auth.js';
+
 import BaseButton from '@/components/global/BaseButton.vue';
 
 export default defineComponent({
   name: 'NavButtonContainer',
   components: {
     BaseButton
+  },
+  props: {
+    isLoggedIn: {
+      type: Boolean
+    }
+  },
+  methods: {
+    ...mapActions(useAuthStore, ['logout']),
+    async onLogout() {
+      try {
+        await this.logout();
+      } catch (error) {
+        alert(error);
+        console.error(error);
+      }
+    }
   }
 });
 </script>
 
 <template>
   <div class="container">
-    <BaseButton
-      class="container__button"
-      :to="{ name: 'Register' }"
-      colorScheme="red-text"
-      size="large"
-    >
-      Register</BaseButton
-    >
-    <BaseButton
-      class="container__button"
-      :to="{ name: 'Login' }"
-      colorScheme="red-fill"
-      size="large"
-    >
-      Login
-    </BaseButton>
+    <template v-if="!isLoggedIn">
+      <BaseButton
+        class="container__button"
+        :to="{ name: 'Register' }"
+        colorScheme="red-text"
+        size="large"
+      >
+        Register</BaseButton
+      >
+      <BaseButton
+        class="container__button"
+        :to="{ name: 'Login' }"
+        colorScheme="red-fill"
+        size="large"
+      >
+        Login
+      </BaseButton>
+    </template>
+    <template v-if="isLoggedIn">
+      <BaseButton
+        class="container__button"
+        :to="{ name: 'Home' }"
+        colorScheme="pink-BG"
+        size="large"
+      >
+        My Account</BaseButton
+      >
+      <BaseButton
+        class="container__button"
+        colorScheme="gray-fill"
+        size="large"
+        @click="onLogout"
+      >
+        Logout
+      </BaseButton>
+    </template>
   </div>
 </template>
 
