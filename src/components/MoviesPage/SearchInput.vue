3314061a0926a6198531cc838b474fbc2bcad1ff
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-08 14:01:24 +0200

    create SearchInput component

---
+++ src/components/MoviesPage/SearchInput.vue
@@ -0,0 +1,49 @@
+<script>
+import { defineComponent } from 'vue';
+
+import BaseTextInput from '@/components/global/BaseTextInput.vue';
+import SearchIcon from '@/assets/icons/search-icon.svg';
+
+export default defineComponent({
+  name: 'PasswordInput',
+  components: {
+    BaseTextInput,
+    SearchIcon
+  },
+  props: {
+    modelValue: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      inputValue: ''
+    };
+  },
+  watch: {
+    inputValue() {
+      this.$emit('update:modelValue', this.inputValue);
+    }
+  },
+  emits: ['focus', 'input', 'update:modelValue']
+});
+</script>
+
+<template>
+  <div class="search-input" v-bind="$attrs">
+    <BaseTextInput
+      :type="$attrs.type"
+      v-model="inputValue"
+      :id="$attrs.id"
+      :required="$attrs.required"
+      :placeholder="$attrs.placeholder"
+    >
+      <template #label> search </template>
+      <template #icon>
+        <SearchIcon />
+      </template>
+    </BaseTextInput>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
