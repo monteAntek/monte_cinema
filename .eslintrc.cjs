f8807872b7217ce60c3eb8eb0dec54c69dfd43db
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-15 14:27:23 +0200

    add typescript to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
     'plugin:prettier/recommended'
   ],
   plugins: ['prettier'],
