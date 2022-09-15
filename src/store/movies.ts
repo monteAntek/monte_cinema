9db45565ef25903f68baf55633f6af49d0f774e1
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-15 14:29:55 +0200

    change movies.js to movies.ts after adding TS to project

--- src/store/movies.ts
+++ src/store/movies.ts
@@ -2,16 +2,27 @@ import { defineStore } from 'pinia';
 
 import { getAllMovies } from '@/api/movies';
 
+import type { Movie } from '@/types/movie';
+
+interface State {
+  allMovies: Movie[] | [];
+}
+
 export const useMoviesStore = defineStore({
   id: 'moviesStore',
   state: () => {
     return {
       allMovies: []
-    };
+    } as State;
   },
   getters: {
-    genres() {
-      return [...new Set(this.allMovies.map((movie) => movie.genre.name))];
+    genres(): string[] {
+      return [
+        ...new Set(this.allMovies.map((movie: Movie) => movie.genre.name))
+      ];
+    },
+    titles(): string[] {
+      return [...new Set(this.allMovies.map((movie: Movie) => movie.title))];
     }
   },
   actions: {
