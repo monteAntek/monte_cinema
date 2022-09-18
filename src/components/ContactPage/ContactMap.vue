f809761dd008080e54e667b080c0bcea2dfa9496
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-18 13:55:37 +0200

    add alt for img
    add styling

--- src/components/ContactPage/ContactMap.vue
+++ src/components/ContactPage/ContactMap.vue
@@ -2,8 +2,26 @@
 
 <template>
   <div class="map">
-    <img src="@/assets/images/contact-map.png" alt="" srcset="" />
+    <img
+      src="@/assets/images/contact-map.png"
+      alt="Monte Cinema Location map"
+    />
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.map {
+  img {
+    object-fit: cover;
+    height: 412px;
+    width: 100%;
+    display: block;
+    border-radius: 8px 0 0 8px;
+
+    @include screen-small {
+      border-radius: 8px 8px 0 0;
+      height: 284px;
+    }
+  }
+}
+</style>
