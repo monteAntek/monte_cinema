b8434e54faf7c9c5ef4b81c4bd1c0e0b8b1ad323
Antoni Karlsson <antoni.karlsson@monterail.com>
2022-09-18 13:52:10 +0200

    move layout to separate ContactSection component

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-import ContactMap from '@/components/ContactPage/ContactMap.vue';
-import ContactDetails from '@/components/ContactPage/ContactDetails.vue';
-import ContactIconPanel from '@/components/ContactPage/ContactIconPanel.vue';
-import BaseHeader from '../components/global/BaseHeader.vue';
+import ContactSection from '@/components/ContactPage/ContactSection.vue';
 </script>
 
 <template>
   <section class="contact">
-    <BaseHeader class="contact__header" header-text="Contact us" />
-    <div class="contact__body">
-      <ContactMap class="contact__body__map" />
-      <ContactDetails class="contact__body__details" />
-      <ContactIconPanel class="contact__body__icons" />
-    </div>
+    <ContactSection />
   </section>
 </template>
 
-<style lang="scss" scoped>
-.contact {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  &__body {
-    display: flex;
-    @include screen-small {
-      margin: 0 24px;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
