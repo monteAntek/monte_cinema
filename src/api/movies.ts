9ae7e680027814703b569ba1a0bd6c2d8bd4fb91
monteAntek <110823711+monteAntek@users.noreply.github.com>
2022-10-04 20:31:22 +0200

    -Refactor api/movies to ts

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,11 +1,14 @@
 import { defaultClient } from '@/api/client';
+import type { Movie } from '@/types/movie';
 
-export const getAllMovies = async () => {
+export const getAllMovies = async (): Promise<Movie[]> => {
   const { data } = await defaultClient.get('/movies');
   return data;
 };
 
-export const getMovieDetails = async (id) => {
+export const getMovieDetails = async (
+  id: string | string[]
+): Promise<Movie> => {
   const { data } = await defaultClient.get(`/movies/${id}`);
   return data;
 };
