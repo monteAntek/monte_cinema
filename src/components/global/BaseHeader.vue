230c6f76997db6346c723c5a269c597928c06a1c
monteAntek <110823711+monteAntek@users.noreply.github.com>
2022-10-04 20:37:48 +0200

    [BaseHeader] -Add size prop + prop based classes

--- src/components/global/BaseHeader.vue
+++ src/components/global/BaseHeader.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 interface Props {
   headerText: string;
   subHeaderText?: string;
+  size?: string;
 }
 
-withDefaults(defineProps<Props>(), {
-  headerText: 'Header'
+const props = withDefaults(defineProps<Props>(), {
+  headerText: 'Header',
+  size: 'large'
+});
+
+const sizeClasses = computed(() => {
+  return [
+    { 'header--large': props.size === 'large' },
+    { 'header--medium': props.size === 'medium' },
+    { 'header--small': props.size === 'small' }
+  ];
 });
 </script>
 
 <template>
-  <section class="header">
+  <section class="header" :class="sizeClasses">
     <h1 class="header__main">{{ headerText }}</h1>
     <h2 v-if="subHeaderText" class="header__sub">{{ subHeaderText }}</h2>
   </section>
@@ -18,18 +30,33 @@ withDefaults(defineProps<Props>(), {
 
 <style lang="scss" scoped>
 .header {
-  margin-top: 124px;
-  margin-bottom: 40px;
-  &__main,
-  &__sub {
-    align-self: flex-start;
-    font-family: $font-eczar;
-    font-weight: $fw-600;
+  align-self: flex-start;
+  font-family: $font-eczar;
+  font-weight: $fw-600;
+  line-height: 1.02;
+  letter-spacing: -0.01em;
+
+  &--large {
     font-size: $fs-80;
-    line-height: 1.02;
-    letter-spacing: -0.01em;
+
     @include screen-small {
-      font-size: $fs-40;
+      font-size: $fs-48;
+    }
+  }
+
+  &--medium {
+    font-size: $fs-64;
+
+    @include screen-small {
+      font-size: $fs-48;
+    }
+  }
+
+  &--small {
+    font-size: $fs-32;
+
+    @include screen-small {
+      font-size: $fs-24;
     }
   }
 
