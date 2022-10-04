cdc616516b7ef7044a20ab0e993ca259c88432b2
monteAntek <110823711+monteAntek@users.noreply.github.com>
2022-10-04 20:42:25 +0200

    [ContactPage] -Add BreadCrumbs component

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 import ContactSection from '@/components/ContactPage/ContactSection.vue';
+import BreadCrumbs from '@/components/global/BreadCrumbs.vue';
+
+const breadCrumbs = computed(() => {
+  return [{ label: 'Contact' }];
+});
 </script>
 
 <template>
   <section class="contact">
+    <BreadCrumbs :crumbs="breadCrumbs" />
     <ContactSection />
   </section>
 </template>
